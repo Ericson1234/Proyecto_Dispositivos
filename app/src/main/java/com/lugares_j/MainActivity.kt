2d47cd9dd705ded1eae0f80cd2a7230cd6aa7718
Ericson1234 <ericsontq06@gmail.com>
2022-10-09 18:09:10 -0600

    Semana 1

--- app/src/main/java/com/lugares_j/MainActivity.kt
+++ app/src/main/java/com/lugares_j/MainActivity.kt
@@ -2,11 +2,25 @@ package com.lugares_j
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugares_j.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Definicion del objeto para hacer la autenticacion
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        //aca haremos algo en el video de la clase...
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
     }
 }
